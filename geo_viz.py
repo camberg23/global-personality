933242fd4150487aa7ddc87af97d15dd225a9634
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2023-08-23 15:22:58 -0400

    Update geo_viz.py

--- geo_viz.py
+++ geo_viz.py
@@ -26,7 +26,7 @@ trait_names = {
 
 traits = ['o', 'c', 'e', 'a', 'n']
 
-col1, col2, col3 = st.beta_columns(3)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     us_or_global = st.selectbox('Data Scope:', ['Choose an option', 'US only', 'Global'])
