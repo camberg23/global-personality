bdfc7c61378e50e48b404a37236fdf8cc260c3be
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2023-09-26 16:51:19 -0400

    Update geo_viz.py

--- geo_viz.py
+++ geo_viz.py
@@ -492,7 +492,6 @@ openai.api_key = st.secrets['KEY']
 
 
 st.set_page_config(page_title="Personality Atlas", layout="wide")
-st.write(" ")
 st.markdown(
     """
     <div style="text-align: center;">
@@ -505,26 +504,31 @@ st.markdown(
     """, 
     unsafe_allow_html=True
 )
-
+st.write(" ")
 st.markdown(
     """
+    <span style="font-size: 1.1em;">
     Welcome to Truity’s **Big Five Personality Atlas**, where you can explore and compare the Big Five 
     [personality traits](https://en.wikipedia.org/wiki/Big_Five_personality_traits) from across the globe, 
     powered by Truity's 4M person database. This page is split up into three different tools: 
-    (1) an interactive personality map, (2) a personality profile generator by location, and 
-    (3) a head-to-head comparison tool. You can take [Truity’s validated Big Five personality assessment 
+    (1) an **interactive personality map**, (2) a **personality profile generator** by location, and 
+    (3) a **head-to-head comparison tool**. You can take [Truity’s validated Big Five personality assessment 
     here](https://www.truity.com/test/big-five-personality-test). The code and data used for generating 
     these analyses is [publicly available](https://github.com/camberg23/global-personality). 
+    </span>
     """,
     unsafe_allow_html=True
 )
 
-st.markdown("""
+st.markdown(
+    """
+    <span style="font-size: 1.1em;">
     Longstanding evidence suggests that the Big Five is a valid measure of personality cross-culturally
     [[1](https://journals.sagepub.com/doi/10.1177/0022022198291009),[2](https://journals.sagepub.com/doi/abs/10.1177/0022022106297299)]. 
     See [here](https://en.wikipedia.org/wiki/Big_Five_personality_traits_and_culture) for an accessible 
-    overview of this subarea of psychometric research. 
-    """,
+    overview of this subarea of psychometric research.
+    </span>
+    """, 
     unsafe_allow_html=True
 )
 
@@ -539,11 +543,7 @@ st.markdown(
     """, 
     unsafe_allow_html=True
 )
-
-
 st.write("---")
-st.write("---")
-
 st.title("Interactive personality maps")
 st.write("specific directions for this bit")
 
